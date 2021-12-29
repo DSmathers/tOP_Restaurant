19ffd2b1ee31666720baf6ed3ac5250daf0d8560
dsmathers <dsmathers91@gmail.com>
2021-12-28 17:10:45 -0800

    added contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 const ContactPage = () => {
     return (
-        <div>
-            <h3>contact page</h3>
-        </div>
+        <section id="contact-page">
+            <header>Contact Us</header>
+            <address id="contact-info">
+                <p className='contact-info-heads'>Address:</p>
+                10101 N Winning Ave.<br />
+                Portland, OR 97206<br />
+                <br />
+                <p className='contact-info-heads'>Phone Number:</p>
+                503-555-5555<br />
+                <p className='contact-info-heads'>Hours of Operation</p>
+                Monday - Friday: 8AM - 10PM<br />
+                Saturday: 10AM - 11PM<br />
+                Sunday: Closed<br /> 
+
+            </address>
+        </section>
     )
 }
 
